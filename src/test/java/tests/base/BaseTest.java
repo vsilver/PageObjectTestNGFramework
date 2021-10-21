6eb9ca37869c828729503ab4eb912bce6b402e8d
Valerii Silvanovych <valerii.silvanovych@intellias.com>
2021-10-21 17:11:40 +0300

    Real Page

--- src/test/java/tests/base/BaseTest.java
+++ src/test/java/tests/base/BaseTest.java
@@ -1,4 +1,14 @@
 package tests.base;
 
+import common.CommonActions;
+import org.openqa.selenium.WebDriver;
+import pages.base.BasePage;
+import pages.realt_home.RealtHomePage;
+
 public class BaseTest {
+    protected WebDriver driver = CommonActions.createDriver();
+    protected BasePage basePage = new BasePage(driver);
+    protected RealtHomePage realtHomePage = new RealtHomePage(driver);
+
+
 }
